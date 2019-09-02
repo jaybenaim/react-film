e50e4991c2bf3e837acde808c8114c605c79285a
jaybenaim <benaimjacob@gmail.com>
2019-09-01 22:08:52 -0400

    fave highlighting working again able to add to list filter needs fixing

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.setCurrent = this.setCurrent.bind(this);
+    // this.handleFaveToggle = this.handleFaveToggle.bind(this);
   }
   filmListingTitles = [];
   initialListings = TMDB["films"].map(film => {
@@ -29,13 +30,14 @@ class App extends Component {
   handleFaveToggle = film => {
     const filmIndex = this.state.newFaves.indexOf(film);
     const isThere = this.state.newFaves.includes(film);
+    console.log(film);
     isThere
       ? this.state.newFaves.pop(filmIndex)
       : this.state.newFaves.push(film);
+
     this.setState({ faves: this.state.newFaves });
   };
   handleFilterClick = filter => {
-    console.log(filter);
     filter === "all"
       ? this.state.newListings.push(this.state.listings)
       : this.setState({ newListings: this.state.newFaves });
@@ -58,7 +60,7 @@ class App extends Component {
 
         <div className="film-details">
           <h1 className="section-title">DETAILS</h1>
-          <FilmDetails listings={this.state.listings} />
+          <FilmDetails listings={this.state.faves} />
         </div>
       </div>
     );
--- src/Fave/index.js
+++ src/Fave/index.js
@@ -4,32 +4,25 @@ class Fave extends Component {
     super(props);
     this.handleFave = this.handleFave.bind(this);
   }
+
   state = {
     isFave: false
   };
   handleFave = e => {
-    e.stopPropagation();
-    this.props.onFaveToggle();
+    this.props.onFaveToggle(this.props.listing);
     this.setState({ isFave: !this.state.isFave });
-
-    // this.state.isFave
-    //   ? this.setState({ isFave: false })
-    //   : this.setState({ isFave: true });
     console.log(this.state);
   };
   render() {
     const isFave = this.state.isFave ? "remove_from_queue" : "add_to_queue";
     const faveClassName = `film-row-fave ${isFave}`;
-
+    console.log(isFave);
     return (
-      <div onClick={this.handleFave}>
-        <div
-          className={faveClassName}
-          onClick={() => this.props.onFaveToggle(this.props.listing)}
-        >
-          <p className="material-icons">add_to_queue</p>
-        </div>
+      // <div onClick={this.handleFave}>
+      <div className={faveClassName} onClick={() => this.handleFave()}>
+        <p className="material-icons">add_to_queue</p>
       </div>
+      // </div>
     );
   }
 }
